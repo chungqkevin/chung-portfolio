e7fcb83e8c3763f60cfa76b1302ec92155f8c58d
Kevin Chung <chung.q.kevin@gmail.com>
2024-03-30 13:30:10 -0700

    minor changes

--- src/components/about.js
+++ src/components/about.js
@@ -9,14 +9,14 @@ function About() {
       <LineSpacer/>
       <br/>
       <p>
-        Welcome and thanks for viewing my portfolio. I'm Kevin Chung, a Developer/Project Manager with backgrounds in computer science, data analytics, and fashion specific e-commerce.
+        Welcome, and thank you for viewing my portfolio. I'm Kevin Chung, a Software Developer/Project Manager with backgrounds in computer science, data analytics, and fashion-specific e-commerce.
         <br /><br />
-        Throughout my career, I have worn several hats, ranging from building custom web applications and search engine optimization (SEO) strategies, to cultivating relationships with designers and producing collaborative projects.
+        Throughout my career, I have taken on various roles, spanning from building custom web applications and implementing search engine optimization (SEO) strategies to fostering relationships with designers and producing collaborative projects.
         <br /><br />
         During my time at Blue Owl, I led a variety of technical projects from concept to deployment, which you will find detailed in my works section below. My diverse tech stack, combined with my education in CS/Data Analytics, has equipped me to make strategic decisions based on facts and metrics.
         <br /><br />
         In my projects, I aim to convey a narrative through code and design with data serving as the foundation.<br/>
-        I am passionate about life-long learning. As an engineer learning new-technologies has always excited me as I strive to better my craft everyday. The ability to learn in both life and work has proven to keep me grounded in every step I take.
+        I am deeply passionate about lifelong learning. As an engineer learning new-technologies has always excited me as I strive to better my craft everyday. The ability to learn in both life and work has proven to keep me grounded in every step I take.
         <br /><br />
         When I'm not working on projects or coding you can find me spending time with my cat, Miso and dog, Gamja.<br/>We enjoy basketball, photography, and sunny days.
         <br /><br />
