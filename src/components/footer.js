a8d06e119990626646e715134f248e61508b070f
Kevin Chung <chung.q.kevin@gmail.com>
2025-01-14 15:28:54 -0800

    more works

--- src/components/footer.js
+++ src/components/footer.js
@@ -5,7 +5,7 @@ import '../styles/base.css';
 function Footer() {
   return (
     <footer>
-        <p>© Kevin Chung 2024 - chung.q.kevin@gmail.com | <a id = "footer-link" href="https://www.linkedin.com/in/kevin-chung-a34236123/">LinkedIn</a></p>
+        <p>© Kevin Chung 2025 - chung.q.kevin@gmail.com | <a id = "footer-link" href="https://www.linkedin.com/in/kevin-chung-a34236123/">LinkedIn</a></p>
     </footer>
   );
 }
